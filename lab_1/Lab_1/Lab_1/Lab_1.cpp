185102144e4cbd8787bb140570609c9422288ec9
arbitew <121195731+arbitew@users.noreply.github.com>
2023-12-09 17:05:31 +0300

    Add files via upload

---
+++ lab_1/Lab_1/Lab_1/Lab_1.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+
+using namespace std;
+/*
+ * Вывод простых чисел
+ *
+ * @param mass_len число до которого выводим.
+ */
+ void simpleNums(int const mass_len) { // количество чисел
+    int *nums = new int [mass_len]; // массив чисел
+    for (int i = 1; i < mass_len; i++) { // заполнение массива
+        nums[i] = i + 1;
+    }
+    nums[0] = 0; // единица не является простым числом
+    for (int i = 0; i < mass_len; i++) { // цикл
+        if (nums[i] != 0) { // если число не равно нулю (простое)
+            printf("%d\n", nums[i]); // выводим простое число
+            for (int j = nums[i] * nums[i] - 1; j <= mass_len; j+=nums[i]) { // просматриваем, делятся ли числа на данное простое число
+                nums[j] = 0;
+            }
+        }
+    }   
+}
+
+int main()
+{   
+    simpleNums(100);
+    return 0;
+}
+
+ 
